90c4d29c5754bb2fe3c197253f40d910aa126ab9
Dusan Klinec <dusan.klinec@gmail.com>
2017-05-31 21:39:46 +0200

    requests wrapper decorator

--- keychest/tls_scanner.py
+++ keychest/tls_scanner.py
@@ -17,6 +17,7 @@ from cert_path_validator import PathValidator, ValidationException
 from tls_domain_tools import TlsDomainTools
 
 import logging
+from functools import wraps
 import requests
 import sqlalchemy as salch
 
@@ -54,57 +55,64 @@ class TlsScanner(object):
         :return: 
         """
 
-    def _wrap_requests(self, fnc):
+    def wrap_requests():
         """
-        Calls fnc in try-catch for requests error
-        :param fnc: 
-        :return: fnc (result, err)
+        Function decorator for requests call, wrapping in try-catch, catching eceptions
+        :return:
         """
-        error = None
-        try:
-            res = fnc()
-            return res, None
+        def wrap_requests_decorator(*args):
+            f = args[0]
 
-        except requests.exceptions.ReadTimeout as rte:
-            error = 'READ_TO'
-            self.trace_logger.log(rte)
+            @wraps(f)
+            def wrapper(*args, **kwds):
+                # noinspection PyBroadException
+                self = args[0]
+                error = None
+                try:
+                    res = f(*args, **kwds)
+                    return res, None
 
-        except requests.exceptions.ConnectTimeout as cte:
-            error = 'CONN_TO'
-            self.trace_logger.log(cte)
+                except requests.exceptions.ReadTimeout as rte:
+                    error = 'READ_TO'
+                    self.trace_logger.log(rte)
 
-        except requests.exceptions.SSLError as cte:
-            error = 'SSL_ERR'
-            self.trace_logger.log(cte)
+                except requests.exceptions.ConnectTimeout as cte:
+                    error = 'CONN_TO'
+                    self.trace_logger.log(cte)
 
-        except requests.exceptions.ConnectionError as ce:
-            error = 'CONN_FAIL'
-            logger.debug('Connection error: %s' % ce)
-            self.trace_logger.log(ce)
+                except requests.exceptions.SSLError as cte:
+                    error = 'SSL_ERR'
+                    self.trace_logger.log(cte)
 
-        except requests.exceptions.RequestException as re:
-            error = 'GEN_ERR'
-            logger.debug('Connection error: %s' % re)
-            self.trace_logger.log(re)
+                except requests.exceptions.ConnectionError as ce:
+                    error = 'CONN_FAIL'
+                    logger.debug('Connection error: %s' % ce)
+                    self.trace_logger.log(ce)
 
-        except IOError as ioe:
-            error = 'IO_ERR'
-            logger.debug('IO error: %s' % ioe)
-            self.trace_logger.log(ioe)
+                except requests.exceptions.RequestException as re:
+                    error = 'GEN_ERR'
+                    logger.debug('Connection error: %s' % re)
+                    self.trace_logger.log(re)
 
-        return None, error
+                except IOError as ioe:
+                    error = 'IO_ERR'
+                    logger.debug('IO error: %s' % ioe)
+                    self.trace_logger.log(ioe)
 
+                return None, error
+            return wrapper
+        return wrap_requests_decorator
+
+    @wrap_requests()
     def load_redirect(self, url, **kwargs):
         """
         Loads redirect - tries to connect on the given URL to load redirect.
         :param url: 
         :return: (new_url, error)
         """
-        def fnc():
-            return self.domain_tools.follow_domain_redirect(url, **kwargs)
+        return self.domain_tools.follow_domain_redirect(url, **kwargs)
 
-        return self._wrap_requests(fnc)
-
+    @wrap_requests()
     def req_connect(self, url, **kwargs):
         """
         requests connect
@@ -112,10 +120,7 @@ class TlsScanner(object):
         :param kwargs: 
         :return: result, error
         """
-        def fnc():
-            return requests.get(url, **kwargs)
-
-        return self._wrap_requests(fnc)
+        return requests.get(url, **kwargs)
 
     def err2status(self, err):
         """
