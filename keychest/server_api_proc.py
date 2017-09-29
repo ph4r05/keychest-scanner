29e1dfc613e8ec67a30c11287243a9935375d71e
Dusan Klinec <dusan.klinec@gmail.com>
2017-09-29 17:48:26 +0200

    KCS-32: loading jobs for api processing

--- keychest/server_api_proc.py
+++ keychest/server_api_proc.py
@@ -17,16 +17,27 @@ from redis_queue import RedisQueue
 import redis_helper as rh
 from trace_logger import Tracelogger
 from errors import Error, InvalidHostname, ServerShuttingDown
-from server_jobs import JobTypes, BaseJob, PeriodicJob, PeriodicReconJob, PeriodicIpScanJob, ScanResults
+from server_jobs import JobTypes, BaseJob, PeriodicJob, ScanResults, PeriodicApiProcessJob
 from consts import CertSigAlg, BlacklistRuleType, DbScanType, JobType, CrtshInputType, DbLastScanCacheType, IpType
 from server_module import ServerModule
+from dbutil import DbApiWaitingObjects, DbApiKey
 
 import time
 import json
+import math
+import random
+import datetime
+import types
 import logging
 import threading
 import collections
 from queue import Queue, Empty as QEmpty, Full as QFull, PriorityQueue
+
+
+import sqlalchemy as salch
+from sqlalchemy.orm.query import Query as SaQuery
+from sqlalchemy import case, literal_column
+from sqlalchemy.orm.session import make_transient
 
 
 logger = logging.getLogger(__name__)
@@ -65,243 +76,80 @@ class ServerApiProc(ServerModule):
         """
         super(ServerApiProc, self).run()
 
-    #
-    # Interface - Redis interactive jobs
-    #
-
-    def process_redis_job(self, job):
+    def load_active_requests(self, s, last_scan_margin=300, randomize=True):
         """
-        Main redis job processor
-        Handles job logic as implemented in Laravel.
-        e.g., removes jobs from delay/reserved queues when finished.
-        :param job:
+        Loads active API requests to process, from the oldest.
+        After loading the result is a tuple (DbIpScanRecordUser, min_periodicity).
+
+        :param s : SaQuery query
+        :type s: SaQuery
+        :param last_scan_margin: margin for filtering out records that were recently processed.
+        :param randomize:
         :return:
         """
-        try:
-            # Process job in try-catch so it does not break worker
-            logger.info('New job: %s' % json.dumps(job.decoded, indent=4))
-            rh.mark_failed_if_exceeds(job)
+        q = s.query(DbApiWaitingObjects) \
+            .join(DbApiKey, DbApiKey.id == DbApiWaitingObjects.api_key_id)\
+            .filter(DbApiWaitingObjects.finished_at == None)\
+            .filter(DbApiWaitingObjects.approval_status == 0)
 
-            # Here we will fire off the job and let it process. We will catch any exceptions so
-            # they can be reported to the developers logs, etc. Once the job is finished the
-            # proper events will be fired to let any listeners know this job has finished.
-            self.on_redis_job(job)
+        if last_scan_margin:
+            if randomize:
+                fact = randomize if isinstance(randomize, types.FloatType) else self.server.randomize_feeder_fact
+                last_scan_margin += math.ceil(last_scan_margin * random.uniform(-1 * fact, fact))
+            cur_margin = datetime.datetime.now() - datetime.timedelta(seconds=last_scan_margin)
 
-            # Once done, delete job from the queue
-            if not job.is_deleted_or_released():
-                job.delete()
+            q = q.filter(salch.or_(
+                DbApiWaitingObjects.last_scan_at < cur_margin,
+                DbApiWaitingObjects.last_scan_at == None
+            ))
 
-        except Exception as e:
-            logger.error('Exception in processing job %s' % (e,))
-            self.trace_logger.log(e)
+        return q.group_by(DbApiWaitingObjects.id) \
+            .order_by(DbApiWaitingObjects.last_scan_at)  # select the oldest scanned first
 
-            rh.mark_failed_exceeds_attempts(job, 5, e)
-            if not job.is_deleted_or_released() and not job.failed:
-                job.release()
-
-    def on_redis_job(self, job):
+    def periodic_feeder(self, s):
         """
-        Main redis job router. Determines which command should be executed.
-        :param job:
+        Feed jobs for processing to the queue
+        :param s:
         :return:
         """
-        payload = job.decoded
-        if payload is None or 'data' not in payload:
-            logger.warning('Invalid job detected: %s' % json.dumps(payload))
-            job.delete()
+        if self.server.periodic_queue_is_full():
             return
 
-        data = payload['data']
-        cmd = data['commandName']
-        if cmd == 'App\\Jobs\\TesterJob':
-            self.on_redis_test_job(job)
-        else:
-            logger.warning('Unknown job: %s' % cmd)
-            job.delete()
+        try:
+            min_scan_margin = self.server.min_scan_margin()
+            query = self.load_active_requests(s, last_scan_margin=min_scan_margin)
+            iterator = query.yield_per(100)
+            for x in iterator:
+                watch_target, min_periodicity, watch_service = x
+
+                if self.server.periodic_queue_is_full():
+                    return
+
+                job = PeriodicApiProcessJob(target=watch_target, periodicity=min_periodicity)
+                self.server.periodic_add_job(job)
+
+        except QFull:
+            logger.debug('Queue full')
             return
 
-    def augment_redis_scan_job(self, job=None, data=None):
-        """
-        Augments job with retry counts, timeouts and so on.
-        :param RedisJob job:
-        :param data:
-        :return:
-        """
-        if job is not None:
-            data = job.decoded['data']['json']
-
-        scan_type = None
-        if 'scan_type' in data:
-            scan_type = data['scan_type']
-
-        sys_params = collections.OrderedDict()
-        sys_params['retry'] = 1
-        sys_params['timeout'] = 4
-        sys_params['mode'] = JobType.UI
-
-        if scan_type == 'planner':
-            sys_params['retry'] = 2
-            sys_params['timeout'] = 15  # tls & connect scan
-            sys_params['mode'] = JobType.BACKGROUND
-
-        data['sysparams'] = sys_params
-        return data
-
-    def load_redis_job(self):
-        """
-        Loads redis job from the queue. Blocking behavior for optimized performance
-        :return:
-        """
-        job = self.redis_queue.pop(blocking=True, timeout=1)
-        if job is None:
-            raise QEmpty()
-
-        return job
-
-    def scan_redis_jobs(self):
-        """
-        Blocking method scanning redis jobs.
-        Should be run in dedicated thread or in the main thread as it blocks the execution.
-        :return:
-        """
-        cur_size = self.redis_queue.size()
-        logger.info('Redis total queue size: %s' % cur_size)
-
-        while self.is_running():
-            job = None
-            try:
-                job = self.load_redis_job()
-
-            except QEmpty:
-                time.sleep(0.01)
-                continue
-
-            try:
-                self.job_queue.put(('redis', job))
-
-            except Exception as e:
-                logger.error('Exception in processing job %s' % (e,))
-                self.trace_logger.log(e)
-
-            finally:
-                pass
-        logger.info('Queue scanner terminated')
-
-    #
-    # Email scanning
-    #
-
-    def process_email_job(self, job):
-        """
-        Processes email job by the worker
-        :param job:
-        :return:
-        """
-        # TODO: analyze email contents
-        # TODO: fingerprint SMIME / PGP
-        # TODO: send email with the results
-        # TODO: move email to the DONE folder
-
-    def main_scan_emails(self):
-        """
-        Main thread for scanning email inbox for jobs.
-        On job is processed, move email to PROGRESS folder.
-        If job is in the progress folder for too long, expire it.
-        :return:
-        """
-        while self.is_running():
-            job = None
-            try:
-                # TODO: load new emails, create job from it
-                pass
-
-            except QEmpty:
-                time.sleep(0.01)
-                continue
-
-            try:
-                self.job_queue.put(('email', job))
-
-            except Exception as e:
-                logger.error('Exception in processing job %s' % (e,))
-                self.trace_logger.log(e)
-
-            finally:
-                pass
-        logger.info('Email scanner terminated')
-
-    #
-    # Running
-    #
-
-    def worker_main(self, idx):
-        """
-        Worker main entry method - worker thread executes this.
-        Processes job_queue jobs, redis enqueued / email queue.
-
-        :param idx:
-        :return:
-        """
-        self.local_data.idx = idx
-        logger.info('Worker %02d started' % idx)
-
-        while self.is_running():
-            job = None
-            try:
-                job = self.job_queue.get(True, timeout=1.0)
-            except QEmpty:
-                time.sleep(0.1)
-                continue
-
-            try:
-                # Process job in try-catch so it does not break worker
-                logger.info('[%02d] Processing job' % (idx, ))
-                jtype, jobj = job
-                if jtype == 'redis':
-                    self.process_redis_job(jobj)
-                elif jtype == 'email':
-                    self.process_email_job(jobj)
-                else:
-                    pass
-
-            except Exception as e:
-                logger.error('Exception in processing job %s: %s' % (e, job))
-                self.trace_logger.log(e)
-
-            finally:
-                self.job_queue.task_done()
-        logger.info('Worker %02d terminated' % idx)
-
-    def on_redis_test_job(self, job):
-        """
-        Redis job for key test - run in the worker
-        :param job:
-        :return:
-        """
-        self.augment_redis_scan_job(job)
-
-        # TODO: extract info to test
-        job_data = job.decoded['data']['json']
-        assoc_id = job_data['id']
-
-        s = None
-        try:
-            # TODO: do the test
-            # TODO: pass the result of the test in the event
-            pass
-            # s = self.db.get_session()
-            #
-            # assoc = s.query(DbSubdomainWatchAssoc).filter(DbSubdomainWatchAssoc.id == assoc_id).first()
-            # if assoc is None:
-            #     return
-
-            # self.auto_fill_assoc(s, assoc)
-            # s.commit()
-
         except Exception as e:
-            logger.warning('Tester job exception: %s' % e)
+            s.rollback()
+            logger.error('Exception loading watch jobs %s' % e)
             self.trace_logger.log(e)
+            raise
 
-        finally:
-            util.silent_close(s)
+    def process_periodic_job(self, job):
+        """
+        Process my jobs in the worker thread.
+        :param job:
+        :return:
+        """
+        if not isinstance(job, PeriodicApiProcessJob):
+            return False
+
+        # TODO: process the job in the worker
+        return True
+
+
+
 
