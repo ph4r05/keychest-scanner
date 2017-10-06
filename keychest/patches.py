de3dc0fa028c1ae2dca697ed71d80cb15126a95e
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-06 10:24:26 +0200

    KCS-34: patches file with eventlet - may be needed later

---
+++ keychest/patches.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import eventlet
+
+# Patching should come first before any other imports.
+# Uncomment to patch stdlib with eventlet.
+# Import this file `patches` before any other.
+
+# eventlet.monkey_patch()
+
+
