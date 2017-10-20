3eb82451302f4fa26b5dd3df233d480ded92fcde
Dusan Klinec <dusan.klinec@gmail.com>
2017-10-20 11:03:15 +0200

    dev - 0.1.14 one version ahead

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.1.13'
+version = '0.1.14'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
