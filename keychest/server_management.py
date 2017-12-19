baafecea8f90c51504777bdb69c13d13892c6ab5
Dusan Klinec <dusan.klinec@gmail.com>
2017-12-19 15:01:43 +0100

    KCS-34: managed test sync

--- keychest/server_management.py
+++ keychest/server_management.py
@@ -7,6 +7,7 @@ Key management scanning, renewal
 
 from past.builtins import basestring    # pip install future
 from past.builtins import cmp
+from future.utils import iteritems
 
 import util
 from config import Config
@@ -31,6 +32,8 @@ import imaplib
 import email
 import email.message as emsg
 from queue import Queue, Empty as QEmpty, Full as QFull, PriorityQueue
+
+import sqlalchemy as salch
 
 
 logger = logging.getLogger(__name__)
@@ -122,26 +125,67 @@ class ManagementModule(ServerModule):
                 # iterate over all associated hosts
                 # sync
                 for sol in DbHelper.yield_limit(q_sol, DbManagedSolution.id):  # type: DbManagedSolution
+                    if sol.deleted_at is not None:
+                        continue
 
                     for svc in [x.service for x in sol.services]:  # type: DbManagedService
+                        if svc.deleted_at is not None:
+                            continue
 
                         mgmt_tests = s.query(DbManagedTest)\
                             .filter(DbManagedTest.solution == sol)\
-                            .filter(DbManagedTest.service == sol)\
+                            .filter(DbManagedTest.service == svc)\
                             .all()  # type: list[DbManagedTest]
 
-                        mgmt_hosts_tests = {x.host_id : x for x in mgmt_tests}  # type: dict[int, DbManagedTest]
+                        mgmt_hosts_tests = {x.host_id: x for x in mgmt_tests}  # type: dict[int, DbManagedTest]
+                        mgmt_hosts_tests_enabled = {x: y for x, y in iteritems(mgmt_hosts_tests) if y.deleted_at is None}
                         all_hosts = {}  # type: dict[int, DbManagedHost]
 
                         for grp in [x.group for x in svc.groups]:  # type: DbHostGroup
+                            if grp.deleted_at is not None:
+                                continue
+
                             for host in [x.host for x in grp.hosts]:  # type: DbManagedHost
+                                if host.deleted_at is not None:
+                                    continue
+
                                 all_hosts[host.id] = host
 
                         # Sync with host tests. For all new host add corresponding test record
                         # And for each test record in the DB
+                        new_hosts_add = [x for x in all_hosts.values()
+                                         if x.id not in mgmt_hosts_tests
+                                         and x.id not in mgmt_hosts_tests_enabled]
 
+                        tests_enable = [x for x in mgmt_hosts_tests.values()
+                                        if x.deleted_at is not None
+                                        and x.host_id is not None
+                                        and x.host_id in all_hosts]
 
-                pass
+                        tests_disable = [x for x in mgmt_hosts_tests.values()
+                                         if x.deleted_at is not None
+                                         and x.host_id is not None
+                                         and x.host_id not in all_hosts]
+
+                        for new_tests in new_hosts_add:
+                            ntest = DbManagedTest()
+                            ntest.host_id = new_tests.id
+                            ntest.solution_id = sol.id
+                            ntest.service_id = svc.id
+                            ntest.created_at = salch.func.now()
+                            s.add(ntest)
+                        s.commit()
+
+                        stmt = salch.update(DbManagedTest)\
+                            .where(DbManagedTest.id.in_([x.id for x in tests_enable])) \
+                            .values(deleted_at=None, updated_at=salch.func.now())
+                        s.execute(stmt)
+
+                        stmt = salch.update(DbManagedTest)\
+                            .where(DbManagedTest.id.in_([x.id for x in tests_disable])) \
+                            .values(deleted_at=salch.func.now(), updated_at=salch.func.now())
+                        s.execute(stmt)
+                        s.commit()
 
             except Exception as e:
                 logger.error('Exception in processing job %s' % (e,))
