a2874212c2cbab59e00927b31ec75c821653fd22
Dusan Klinec <dusan.klinec@gmail.com>
2017-12-22 20:05:38 +0100

    KCS-34: tls cert utils py3 fix

--- keychest/tests/test_cert_utils.py
+++ keychest/tests/test_cert_utils.py
@@ -96,11 +96,11 @@ class CertUtilTest(unittest.TestCase):
         subj_ids = [util.try_get_subject_key_identifier(x, compute_if_not_present=False) for x in certs]
         computed_subj_ids = [util.try_compute_subject_key_identifier(x, False) for x in certs]
 
-        self.assertEqual(util.b16encode(subj_ids[0]),
+        self.assertEqual(util.to_string(util.b16encode(subj_ids[0])),
                          self._ossl('A8:4A:6A:63:04:7D:DD:BA:E6:D1:39:B7:A6:45:65:EF:F3:A8:EC:A1'))
-        self.assertEqual(util.b16encode(subj_ids[1]),
+        self.assertEqual(util.to_string(util.b16encode(subj_ids[1])),
                          self._ossl('FF:FB:9D:B7:99:6C:1E:F6:47:0A:6F:CA:46:DB:91:6C:60:02:BE:94'))
-        self.assertEqual(util.b16encode(subj_ids[2]),
+        self.assertEqual(util.to_string(util.b16encode(subj_ids[2])),
                          self._ossl('7B:52:06:B4:C2:C2:D1:28:CB:71:A4:AC:3A:C1:80:94:57:7C:35:AD'))
 
         self.assertEqual(subj_ids, computed_subj_ids)
@@ -113,11 +113,11 @@ class CertUtilTest(unittest.TestCase):
         certs = [util.load_x509(cert) for cert in self.certs]
         auth_ids = [util.try_get_authority_key_identifier(x) for x in certs]
 
-        self.assertEqual(util.b16encode(auth_ids[0]),
+        self.assertEqual(util.to_string(util.b16encode(auth_ids[0])),
                          self._ossl('C4:A7:B1:A4:7B:2C:71:FA:DB:E1:4B:90:75:FF:C4:15:60:85:89:10'))
-        self.assertEqual(util.b16encode(auth_ids[1]),
+        self.assertEqual(util.to_string(util.b16encode(auth_ids[1])),
                          self._ossl('A8:4A:6A:63:04:7D:DD:BA:E6:D1:39:B7:A6:45:65:EF:F3:A8:EC:A1'))
-        self.assertEqual(util.b16encode(auth_ids[2]),
+        self.assertEqual(util.to_string(util.b16encode(auth_ids[2])),
                          self._ossl('7B:52:06:B4:C2:C2:D1:28:CB:71:A4:AC:3A:C1:80:94:57:7C:35:AD'))
 
     def test_ev(self):
