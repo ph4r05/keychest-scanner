82c8685ad72b2642e6dbfc2348afc151a529e7de
Dusan Klinec <dusan.klinec@gmail.com>
2018-02-01 17:01:30 +0100

    dev: v0.1.18 - one version ahead

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '0.1.17'
+version = '0.1.18'
 
 # Please update tox.ini when modifying dependency version requirements
 install_requires = [
